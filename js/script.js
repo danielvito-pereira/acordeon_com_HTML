8aeaf4950a1f565b889c9a05588e54035577cadb
Daniel Vitor <danielvitor081@gmail.com>
2022-11-23 18:01:27 -0300

    final

---
+++ js/script.js
@@ -0,0 +1,24 @@
+'use strict'
+const bloque = document.querySelectorAll('.bloque');
+const h2 = document.querySelectorAll('.h2');
+
+//Quando click em h2 
+  //tirar a classe activo de todos os bloques 
+  // Adicionar a classe activo ao bloque com a posição do h2 
+
+// Recorrer TODOS los h2
+h2.forEach( ( cadaH2 , i )=>{
+  // Asignando un CLICK a cada h2
+  h2[i].addEventListener('click', ()=>{
+
+      // Recorrer TODOS los bloque
+      bloque.forEach( ( cadaBloque , i )=>{
+          // Quitamos la clase activo de TODOS los bloques
+          bloque[i].classList.remove('activo')
+      })
+      // Añadiendo la clase activo al bloque cuya posición sea igual al del h2
+      // (Línea número 12)
+      bloque[i].classList.add('activo')
+
+  })
+})
